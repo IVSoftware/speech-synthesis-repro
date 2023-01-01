69d9e4164d7f06743f7b539e4505e25fa9fee881
IVSoftware <tgregor@ivsoftware.com>
2023-01-01 08:17:31 -0700

    Add project files.

---
+++ speech-synthesis-repro/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace speech_synthesis_repro
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(66, 26);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.numericUpDown2.Location = new System.Drawing.Point(12, 55);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(66, 26);
+            this.numericUpDown2.TabIndex = 0;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ speech-synthesis-repro/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System;
+using System.Threading.Tasks;
+
+namespace speech_synthesis_repro
+{
+    public partial class MainForm : Form
+    {
+        SpeechSynthesizer _speech = new SpeechSynthesizer();
+        public MainForm() => InitializeComponent();
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            // Make 100% sure that the up-down ctrl is decoupled from speak call.
+            var say = $"{numericUpDown1.Value}";
+            // I believe we're inside `Mousedown` and a `ValueChanged` synchronous
+            // handlers. Let them finish.
+            BeginInvoke((MethodInvoker)delegate
+            {
+                _speech.SpeakAsyncCancelAll();
+                _speech.SpeakAsync(say);
+            });
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            // Make 100% sure that the up-down ctrl is decoupled from speak call.
+            var say = $"{numericUpDown2.Value}";
+            // I don't see why this should make any difference.
+            Task.Run(() =>
+            {
+                _speech.SpeakAsyncCancelAll();
+                _speech.SpeakAsync(say);
+            });
+        }
+    }
+}
